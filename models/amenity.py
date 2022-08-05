b37681a8ae0019e915ec7bac2ccffdf45881b19b
Stephen Muchendu <muchendumaina66@gmail.com>
2022-08-05 20:57:03 +0300

    airBnB

---
+++ models/amenity.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 5 13:28:17 2022
+
+@author: Stephen Muchendu
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Amenity class inherits from BaseModel
+
+    Attribute:
+        name (str): Public class attribute for Amenity's name
+    """
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+        """init method for Amenity class
+
+        Attributes:
+            args (list): The list with arguments
+            kwargs (dict): A dictionary with arguments
+        """
+        super().__init__(*args, **kwargs)
